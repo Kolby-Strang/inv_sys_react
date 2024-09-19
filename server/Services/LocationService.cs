7ec81f7966df47b30677acac9b861ecb821a102c
Kolby-Strang <strangkolby@gmail.com>
2024-09-19 14:56:36 -0600

    small fix

--- server/Services/LocationService.cs
+++ server/Services/LocationService.cs
@@ -2,7 +2,7 @@ namespace inv_sys_react.Services;
 
 public class LocationService
 {
-    private readonly LocationRepository _repo; i
+    private readonly LocationRepository _repo;
 
     public LocationService(LocationRepository repo)
     {
